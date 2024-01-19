25b97f0743b3cfbb479168ae786c2f3f20757d94
Ayush <ayush.thakur@xenonstack.com>
2024-01-19 12:32:07 +0530

    add file for google Deployment Manager

--- execute_function.sh
+++ execute_function.sh
@@ -13,3 +13,5 @@ sls package --verbose
 cp .serverless/*.zip ../
 cd ..
 rm -rf function_code
+gcloud storage cp *.zip gs://function-test-420  
+gcloud deployment-manager deployments create my-first-deployment --config configurable_functions.yaml                 
