e7248978ad5ed66151a0d228ceca36dd99602406
Ayush <ayush.thakur@xenonstack.com>
2024-01-25 14:23:11 +0530

    updated Jenkinsfile and execution script

--- execute_function.sh
+++ execute_function.sh
@@ -16,5 +16,5 @@ gcloud storage cp *.zip gs://function-test-420
 gcloud deployment-manager deployments create my-first-deployment --config configurable_functions.yaml  --async                
 gcloud api-gateway api-configs describe generic-v2 --api=hello-world-api --view=FULL | yq '.openapiDocuments[0].document.contents' | base64 --decode > swagger-v2.yaml
 cat swagger-v2.yaml
-cp swagger-v2.yaml swagger-updated-v2.yaml
-sed 's/function-1/'"$1"'/' swagger-updated-v2.yaml > swagger-updated-v2.yaml
+cp swagger-v2.yaml swaggerUpdatedv2.yaml
+sed 's/function-1/'"$1"'/' swaggerUpdatedv2.yaml > swaggerUpdatedv2.yaml
