0f27bc9d7fa4b5f6b89adbfca0c5ea0c25cd9e4d
Ayush <ayush.thakur@xenonstack.com>
2024-02-17 04:36:50 +0530

    updated gcp integration script

--- execute_function.sh
+++ execute_function.sh
@@ -17,6 +17,7 @@ export stack=${function}
 export gcpfunction=${function}
 export gcsBucket=${CloudStorage}
 envsubst < configurable_functions.yaml > cloud-function.yaml
+export stackname=${function}
 cat cloud-function.yaml
 gcloud storage cp *.zip gs://$2  
-gcloud deployment-manager deployments create {$1} --config cloud-function.yaml  --async                
+gcloud deployment-manager deployments create $[stackname] --config cloud-function.yaml  --async                
