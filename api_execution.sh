3cf2645ddd6b202d7a1f910940ede97fd176dee7
Ayush <ayush.thakur@xenonstack.com>
2024-02-20 02:56:49 +0530

    updated api excution script

--- api_execution.sh
+++ api_execution.sh
@@ -2,6 +2,6 @@
 
 export api=${ApiGateway}
 export Apiconfig=${config}
-gcloud api-gateway api-configs describe generic-config --api=hello-world-api --view=FULL | yq '.openapiDocuments[0].document.contents' | base64 --decode > swaggerv2.yaml
+gcloud api-gateway api-configs describe ${Apiconfig} --api=${api} --view=FULL | yq '.openapiDocuments[0].document.contents' | base64 --decode > swaggerv2.yaml
 cat swaggerv2.yaml
 diff swaggerv2.yaml swagger.yaml --unified=0 || true
